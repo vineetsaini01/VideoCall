7267bb96a2be15f127f3ea3f14b468bf4bea2be3
deepak <vineetsaini01@gmail.com>
2024-03-05 13:30:29 +0530

    new token

--- app/src/main/java/com/example/videocall/utils/Constents.kt
+++ app/src/main/java/com/example/videocall/utils/Constents.kt
@@ -2,7 +2,7 @@ package com.example.videocall.utils
 
 object Constents {
 
-    const val AppID:Long=1923818936
-    const val AppSign="c3e50896e2281743781f1f723ca49bca519d344999db512940c07067ba214394"
+    const val AppID:Long=1016367998
+    const val AppSign="8cecb609b18e9c48f61813f8f37f86d10036a8ad07042c30e37a865dacf8b94a"
 
 }
